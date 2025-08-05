4a2163c4d16e349a797251c4a1192f54d898a5ab
JIHU96 <korjihu@gmail.com>
2025-08-06 00:14:33 +0900

    Updates

---
+++ static/chunks/app/about/page-4d2475ea31a1a6bf.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220,880],{3765:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,4006,23))},4006:()=>{}},e=>{e.O(0,[615,441,964,358],()=>e(e.s=3765)),_N_E=e.O()}]);
